aacf23c57c4c1d2260bc210f61f8f37fd5c74c3d
wangmaoquan <1782976211@qq.com>
2023-01-03 11:24:12 +0800

    feat: 开启增强md功能

--- docs/.vuepress/configs/theme/plugins.ts
+++ docs/.vuepress/configs/theme/plugins.ts
@@ -3,6 +3,24 @@ import { ThemeOptions } from 'vuepress-theme-hope';
 const plugins: ThemeOptions['plugins'] = {
   // 开启博客功能
   blog: true,
+  mdEnhance: {
+    tasklist: true, // 开启任务列表
+    tabs: true, // 开启选项卡
+    mermaid: true,
+    codetabs: true,
+    // 启用 figure
+    figure: true,
+    // 启用图片懒加载
+    imgLazyload: true,
+    // 启用图片标记
+    imgMark: true,
+    // 启用图片大小
+    imgSize: true,
+    include: true,
+    mark: true,
+    flowchart: true,
+    
+  },
 };
 
 export default plugins;
