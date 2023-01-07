8aa6e537f5558811d30d44fe28273c73692a3ac8
wangmaoquan <1782976211@qq.com>
2023-01-07 15:19:33 +0800

    feat: 切换主题色

--- docs/.vuepress/configs/theme/baseConfig.ts
+++ docs/.vuepress/configs/theme/baseConfig.ts
@@ -3,4 +3,10 @@ export const baseConfig: ThemeOptions = {
   repo: 'https://github.com/WangMaoquan/learn-leetcode',
   footer: 'Copyright © 2022-present Decade',
   displayFooter: true,
+  themeColor: {
+    blue: '#2196f3',
+    red: '#f26d6d',
+    green: '#3eaf7c',
+    orange: '#fb9b5f',
+  },
 };
