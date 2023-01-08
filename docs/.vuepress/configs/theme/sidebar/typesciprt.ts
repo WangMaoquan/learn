5fa7674fc7d6b635946b14ac08da0e4b5b35e378
wangmaoquan <1782976211@qq.com>
2023-01-08 14:09:49 +0800

    fix: 修改

--- docs/.vuepress/configs/theme/sidebar/typesciprt.ts
+++ docs/.vuepress/configs/theme/sidebar/typesciprt.ts
@@ -1,3 +1,3 @@
 import { arraySidebar } from 'vuepress-theme-hope';
 
-export const typescriptSideBar = arraySidebar(['', '1']);
+export const typescriptSideBar = arraySidebar(['', '1', '2']);
--- docs/learn-typescript/README.md
+++ docs/learn-typescript/README.md
@@ -6,3 +6,4 @@ article: false
 ### typescript
 
 - [start](1.md)
+- [模式匹配](2.md)
