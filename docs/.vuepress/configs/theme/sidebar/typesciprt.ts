513e7217b15440691399ca69882d6968941c2975
wangmaoquan <1782976211@qq.com>
2023-01-10 12:45:37 +0800

    feat: 文章初始化

--- docs/.vuepress/configs/theme/sidebar/typesciprt.ts
+++ docs/.vuepress/configs/theme/sidebar/typesciprt.ts
@@ -1,3 +1,11 @@
 import { arraySidebar } from 'vuepress-theme-hope';
 
-export const typescriptSideBar = arraySidebar(['', '1', '2', '3', '4', '5']);
+export const typescriptSideBar = arraySidebar([
+  '',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+]);
